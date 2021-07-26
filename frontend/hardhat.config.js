3471d5f770f7377868c71c5601d867aa006b0eb2
lokesh-acharya <lokesh.acharya.526@gmail.com>
2021-07-26 17:21:07 +0530

    draft

--- frontend/hardhat.config.js
+++ frontend/hardhat.config.js
@@ -3,15 +3,15 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { REACT_APP_ALCHEMY_KEY, REACT_APP_PRIVATE_KEY } = process.env;
+const { ALCHEMY_KEY, PRIVATE_KEY } = process.env;
 module.exports = {
    solidity: "0.7.3",
    defaultNetwork: "ropsten",
    networks: {
       hardhat: {},
       ropsten: {
-         url: REACT_APP_ALCHEMY_KEY,
-         accounts: [`0x${REACT_APP_PRIVATE_KEY}`]
+         url: ALCHEMY_KEY,
+         accounts: [`0x${PRIVATE_KEY}`]
       }
    },
 }
